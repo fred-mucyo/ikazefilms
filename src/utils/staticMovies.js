a9e65ec33f0836ded28ba828797bb6e2ef11c470
Remy <shingiroremy303@gmail.com>
2025-08-29 09:57:03 +0200

    Added The equalizer movie

--- src/utils/staticMovies.js
+++ src/utils/staticMovies.js
@@ -809,6 +809,19 @@ const staticMovies = [
     youtube_trailer_url: 'https://youtu.be/i5EognW_9Lk?si=ivlJhAeX4vzrWnf_'
   },
 
+  {
+    id: 's63',
+    title: "The Equalizer 3",
+    description: 'Gripping stories at a glance.',
+    interpreter_name: 'ROCKY',
+    created_at: '2024-08-30T00:00:00.000Z',
+    thumbnail_url: '/equalizer.jpg',
+    poster_url: '/equalizer.jpg',
+    image_url: '/equalizer.jpg',
+    video_url: 'https://hglink.to/l3umbc6d66u5',
+    youtube_trailer_url: 'https://youtu.be/19ikl8vy4zs?si=g7FpnDmkTdaHoBJU'
+  },
+
 
 
 ];
