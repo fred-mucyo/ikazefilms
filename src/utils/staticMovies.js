27302b59064e8a769ca718d55018f176f1568c2f
Remy <shingiroremy303@gmail.com>
2025-08-29 10:00:58 +0200

    Added family plan movie

--- src/utils/staticMovies.js
+++ src/utils/staticMovies.js
@@ -822,6 +822,19 @@ const staticMovies = [
     youtube_trailer_url: 'https://youtu.be/19ikl8vy4zs?si=g7FpnDmkTdaHoBJU'
   },
 
+  {
+    id: 's64',
+    title: "The Family Plan",
+    description: 'Gripping stories at a glance.',
+    interpreter_name: 'ROCKY',
+    created_at: '2024-08-30T00:00:00.000Z',
+    thumbnail_url: '/family.jpg',
+    poster_url: '/family.jpg',
+    image_url: '/family.jpg',
+    video_url: 'https://hglink.to/h38ienf6qfs3',
+    youtube_trailer_url: 'https://youtu.be/ns8weNznn1Y?si=VliD78g8211NZMJm'
+  },
+
 
 
 ];
